589ff728ae9cec4a07b5265be8bf35d7a7637b19
Laa Laa <laalaa6405@gmail.com>
2025-06-22 12:00:00 +0800

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,21 +67,21 @@ const Main = () => {
           className="relative w-full h-screen overflow-hidden flex flex-col justify-center items-center px-16 perspective-[600px]"
         >
           {/* Bottom layer (revealed) */}
-          <div className="absolute inset-0 bg-linear-to-t from-neutral-900 to-black text-white flex items-end justify-start text-4xl font-bold z-10">
+          <div className="absolute inset-0 bg-linear-to-t from-neutral-900 to-black text-white flex items-end justify-start font-jost text-4xl z-10">
             <p ref={paragraphOne}>Lorem ipsum dolor, sit amet consectetur adipisicing elit. <br />Fugiat deserunt porro ipsa debitis recusandae blanditiis aut, <br />natus illum at molestiae incidunt exercitationem, <br />veritatis iure quia sapiente animi asperiores, cumque quas?</p>
           </div>
           {/* Top layer (clipped away on scroll) */}
           <div
             ref={topLayerRef}
-            className="absolute inset-0 bg-white text-black flex items-end justify-start px-80 py-4 text-4xl font-bold z-20"
+            className="absolute inset-0 bg-white text-black flex items-end justify-start text-4xl font-bold z-20"
             style={{
               clipPath: 'inset(0% 0% 0% 0%)', // fully visible at start
             }}
           >
-            <div className='perspective-[400px]'>
-              <h2 ref={myText} className='transform-3d text-8xl'>My</h2>
-              <h2 ref={portfolioText} className='transform-3d text-8xl'>Portfolio</h2>
-              <h2 ref={showcaseText} className='transform-3d text-8xl'>Showcase</h2>
+            <div className='perspective-[400px] flex flex-col'>
+              <span ref={myText} className='font-jost font-normal transform-3d text-8xl'>My</span>
+              <span ref={portfolioText} className='font-jost font-normal transform-3d text-8xl'>Portfolio</span>
+              <span ref={showcaseText} className='font-jost font-normal transform-3d text-8xl'>Showcase</span>
             </div>
           </div>
           <h1 ref={headingOne} className='z-100 lg:text-9xl md:text-6xl sm:text-3xl font-jost font-normal text-amber-50 mix-blend-difference transform-3d'>hello</h1>
