0d3b9f6cfc4872f905e7b6687ff42c7e1810b182
Laa Laa <laalaa6405@gmail.com>
2025-11-20 10:15:56 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,16 +13,15 @@ import Image from 'next/image';
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const Main = () => {
-
   useGSAP(() => {
     const split = SplitText.create('.split', { type: 'words, chars' });
 
     // now animate the characters in a staggered fashion
     gsap.from(split.chars, {
       duration: 1,
-      y: 100,       // animate from 100px below
+      y: 100, // animate from 100px below
       autoAlpha: 0, // fade in from opacity: 0 and visibility: hidden
-      stagger: 0.05 // 0.05 seconds between each
+      stagger: 0.05, // 0.05 seconds between each
     });
 
     gsap.to('.gallery', {
@@ -31,8 +30,8 @@ const Main = () => {
       ease: 'power2.out',
       scrollTrigger: {
         trigger: '.gallery',
-        start: 'top center'
-      }
+        start: 'top center',
+      },
     });
   }, []);
 
@@ -42,9 +41,7 @@ const Main = () => {
       <section className='max-w-full h-screen flex flex-col justify-center items-center px-4'>
         <div className='max-w-7xl flex flex-col items-start gap-6'>
           <div className='overflow-hidden py-4'>
-            <h1
-              className='font-zalando font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl text-amber-50 split'
-            >
+            <h1 className='font-zalando font-bold text-6xl sm:text-6xl md:text-7xl lg:text-8xl text-amber-50 split'>
               Hello,{' '}
               <span className='inline-block translate-y-3'>
                 <Image
@@ -60,28 +57,36 @@ const Main = () => {
             </h1>
           </div>
           <p className='font-poppins font-light text-amber-50 max-w-2xl'>
-            I&apos;m a Front-end Web Developer and UI/UX Designer from Selangor,
-            Malaysia. Available for work.
+            I&apos;m a Front-end Web Developer and UI/UX Designer from Selangor, Malaysia. Available
+            for work.
           </p>
-          <a href='#' className='font-poppins rounded-4xl px-4 py-1.5 border border-white text-amber-50 font-light flex justify-center items-center gap-1 hover:bg-amber-50 hover:text-[#1b1b1b] transition duration-300 ease-in-out'>Reach me out!<RiArrowRightUpLine className='text-2xl' /></a>
+          <a
+            href='#'
+            className='font-poppins rounded-4xl px-4 py-1.5 border border-white text-amber-50 font-light flex justify-center items-center gap-1 hover:bg-amber-50 hover:text-[#1b1b1b] transition duration-300 ease-in-out'
+          >
+            Reach me out!
+            <RiArrowRightUpLine className='text-2xl' />
+          </a>
         </div>
       </section>
 
       <section className='container flex justify-center max-w-full pb-30'>
-        <div className='flex flex-col sm:flex-col md:flex-row lg:flex-row justify-between'>
-          <div className='flex flex-col basis-1/2 gap-3'>
+        <div className='flex flex-col sm:flex-col md:flex-col lg:flex-row justify-between w-full'>
+          <div className='flex flex-col gap-3'>
             <span className='uppercase font-zalando text-amber-50 text-[1rem]'>Skills</span>
             <h2 className='font-zalando font-bold text-amber-50 leading-tight max-w-2xl'>
               All those technologies I use
             </h2>
-            <p className='font-poppins font-light text-amber-50 max-w-2xl'>I build things mostly with these — but I love learning new ones.</p>
+            <p className='font-poppins font-light text-amber-50 max-w-2xl'>
+              I build things mostly with these — but I love learning new ones.
+            </p>
           </div>
 
-          <div className='flex flex-col basis-1/2 gap-6'>
+          <div className='flex flex-col gap-6'>
             <div className='flex flex-wrap flex-col gap-4'>
               <h3 className='font-zalando text-amber-50 text-2xl pb-2'>Tech Stack</h3>
 
-              <div className='wrapper fadeout-horizontal'>
+              <div className='wrapper fadeout-horizontal max-w-2xs sm:max-w-2xs lg:max-w-3xl'>
                 <div className='marquee-text'>
                   <ul className='marquee-text-track font-poppins'>
                     <li>HTML</li>
@@ -108,7 +113,7 @@ const Main = () => {
             <div className='flex flex-wrap flex-col gap-4'>
               <h3 className='font-zalando text-amber-50 text-2xl pb-2'>Other Tools</h3>
 
-              <div className='wrapper fadeout-horizontal'>
+              <div className='wrapper fadeout-horizontal max-w-2xs sm:max-w-2xs lg:max-w-3xl'>
                 <div className='marquee-text'>
                   <ul className='marquee-text-track font-poppins'>
                     <li>Figma</li>
@@ -142,25 +147,46 @@ const Main = () => {
             <h2 className='font-zalando font-bold text-amber-50 leading-tight'>
               Explore my comprehensive service offerings
             </h2>
-            <p className='font-poppins font-light text-amber-50 max-w-2xl'>Every service I offer is designed to help you attract, impress, and convert.</p>
+            <p className='font-poppins font-light text-amber-50 max-w-2xl'>
+              Every service I offer is designed to help you attract, impress, and convert.
+            </p>
           </div>
 
           <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6'>
             <div className='border-l border-amber-50 pl-4 basis-1/4 flex flex-col justify-between'>
-              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>/Website<br /> Design</h3>
-              <p className='font-poppins font-light text-amber-50'>I create user-focused interfaces that make navigation effortless and enjoyable, helping visitors stay longer and engage more deeply.</p>
+              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>
+                /Website
+                <br /> Design
+              </h3>
+              <p className='font-poppins font-light text-amber-50'>
+                I create user-focused interfaces that make navigation effortless and enjoyable,
+                helping visitors stay longer and engage more deeply.
+              </p>
             </div>
             <div className='border-l border-amber-50 pl-4 basis-1/4 flex flex-col justify-between'>
               <h3 className='font-zalando text-amber-50 text-2xl pb-2'>/Frontend Development</h3>
-              <p className='font-poppins font-light text-amber-50'>Using modern frameworks like React and Tailwind, I ensure your site loads quickly, adapts perfectly to all screens, and delivers a seamless experience.</p>
+              <p className='font-poppins font-light text-amber-50'>
+                Using modern frameworks like React and Tailwind, I ensure your site loads quickly,
+                adapts perfectly to all screens, and delivers a seamless experience.
+              </p>
             </div>
             <div className='border-l border-amber-50 pl-4 basis-1/4 flex flex-col justify-between'>
-              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>/Performance & Optimization</h3>
-              <p className='font-poppins font-light text-amber-50'>I analyze and fine-tune your site’s speed, SEO, and accessibility to ensure every millisecond counts.</p>
+              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>
+                /Performance & Optimization
+              </h3>
+              <p className='font-poppins font-light text-amber-50'>
+                I analyze and fine-tune your site’s speed, SEO, and accessibility to ensure every
+                millisecond counts.
+              </p>
             </div>
             <div className='border-l border-amber-50 pl-4 basis-1/4 flex flex-col justify-between'>
-              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>/Maintenance & Ongoing Support</h3>
-              <p className='font-poppins font-light text-amber-50'>I provide regular updates, backups, and optimizations so you can focus on growing your business while I handle the tech side.</p>
+              <h3 className='font-zalando text-amber-50 text-2xl pb-2'>
+                /Maintenance & Ongoing Support
+              </h3>
+              <p className='font-poppins font-light text-amber-50'>
+                I provide regular updates, backups, and optimizations so you can focus on growing
+                your business while I handle the tech side.
+              </p>
             </div>
           </div>
         </div>
@@ -170,13 +196,13 @@ const Main = () => {
         <div className='flex flex-col gap-12'>
           <div className='max-w-3xl flex flex-col gap-6'>
             <span className='uppercase font-zalando text-amber-50 text-[1rem]'>Projects</span>
-            <h2 className='font-zalando font-bold text-amber-50 leading-tight'>
-              Featured works
-            </h2>
-            <p className='font-poppins font-light text-amber-50 max-w-2xl'>A selection of projects I’ve designed and built with care and precision.</p>
+            <h2 className='font-zalando font-bold text-amber-50 leading-tight'>Featured works</h2>
+            <p className='font-poppins font-light text-amber-50 max-w-2xl'>
+              A selection of projects I’ve designed and built with care and precision.
+            </p>
           </div>
 
-          <div className='grid grid-cols-2 gap-12'>
+          <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-12'>
             <div className='flex flex-col gap-3'>
               <Image
                 className='shadow-lg w-full aspect-video object-cover gallery'
@@ -190,7 +216,12 @@ const Main = () => {
                 <RiArrowRightUpLine className='text-4xl text-amber-50' />
               </div>
               <div className='flex'>
-                <a href='https://timeless-vacation.com/' className='font-poppins font-light text-amber-50 '>https://timeless-vacation.com/</a>
+                <a
+                  href='https://timeless-vacation.com/'
+                  className='font-poppins font-light text-amber-50 '
+                >
+                  https://timeless-vacation.com/
+                </a>
               </div>
             </div>
 
@@ -207,7 +238,13 @@ const Main = () => {
                 <RiArrowRightUpLine className='text-4xl text-amber-50' />
               </div>
               <div className='flex'>
-                <a href='https://otterbarista.com/' target='_blank' className='font-poppins font-light text-amber-50'>https://otterbarista.com/</a>
+                <a
+                  href='https://otterbarista.com/'
+                  target='_blank'
+                  className='font-poppins font-light text-amber-50'
+                >
+                  https://otterbarista.com/
+                </a>
               </div>
             </div>
           </div>
@@ -220,7 +257,13 @@ const Main = () => {
             <h3 className='font-zalando font-normal text-amber-50'>
               Want something custom? Let’s talk about your idea!
             </h3>
-            <a href='#' className='font-poppins rounded-4xl px-4 py-1.5 border border-white text-amber-50 font-light flex justify-center items-center gap-1 hover:bg-amber-50 hover:text-[#1b1b1b] transition duration-300 ease-in-out'>Reach me out!<RiArrowRightUpLine className='text-2xl' /></a>
+            <a
+              href='#'
+              className='font-poppins rounded-4xl px-4 py-1.5 border border-white text-amber-50 font-light flex justify-center items-center gap-1 hover:bg-amber-50 hover:text-[#1b1b1b] transition duration-300 ease-in-out'
+            >
+              Reach me out!
+              <RiArrowRightUpLine className='text-2xl' />
+            </a>
           </div>
         </div>
       </section>
